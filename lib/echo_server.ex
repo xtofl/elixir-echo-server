0feb8c10b1122b0693d506b842522cdf9a405637
xtofl <xtofl@github.com>
2014-10-05 12:11:15 +0200

    [refac] extract accept function

--- lib/echo_server.ex
+++ lib/echo_server.ex
@@ -2,27 +2,35 @@ defmodule EchoServer do
   def listen(port) do
     tcp_options = [:list, {:packet, 0}, {:active, false}, {:reuseaddr, true}]
     {:ok, listen_socket} = :gen_tcp.listen(port, tcp_options)
-    do_listen(listen_socket)
+    do_listen(listen_socket, :gen_tcp.accept)
   end
- 
-  defp do_listen(listen_socket) do
-    {:ok, socket} = :gen_tcp.accept(listen_socket)
-    spawn(fn() -> do_server(socket) end)
-    do_listen(listen_socket)
+
+  def receive_from(socket) do
   end
- 
+
+  def do_listen(listen_socket, accept) do
+    case accept.(listen_socket) do
+    {:ok, socket} ->
+      accept.(listen_socket)
+      spawn(fn() -> do_server(socket) end)
+      do_listen(listen_socket, accept)
+    {:error} -> {:error}
+    end
+  end
+
   defp do_server(socket) do
     case :gen_tcp.recv(socket, 0) do
       {:ok, data} ->
-	IO.puts "received #{data}"
+        Logger.log "received #{data}"
         responder = spawn(fn() -> do_respond(socket) end)
         spawn(fn() -> send responder, {:ok, data} end)
         do_server(socket)
- 
-      {:error, :closed} -> :ok
+
+      {:error, :closed} -> 
+        Logger.log "error: closed"
     end
   end
- 
+
   defp do_respond(socket) do
     receive do
       {:ok, response} ->
@@ -31,7 +39,7 @@ defmodule EchoServer do
     end
   end
 end
- 
+
 defmodule Logger do
   def log(message) do
     {{year, month, day}, {hour, min, sec}} = :erlang.localtime
--- test/echo_server_test.exs
+++ test/echo_server_test.exs
@@ -1,6 +1,11 @@
 defmodule EchoServerTest do
   use ExUnit.Case
 
+  test "returns error when accept fails" do
+      mock = fn(10) -> {:error} end
+    assert EchoServer.do_listen(10, mock) == {:error}
+  end
+
   test "sends back an echo" do
     assert Echo.reply("hi") == "hi"
   end
